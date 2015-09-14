25face847a8612274f84902d8f1954ea256c6fc3
acorn <acorn@junk>
2015-09-13 20:08:27 -0500

    funcy for loop

--- hello.go
+++ hello.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func mainForLoopFuncy() {
+	var x [5]float64
+	x[0] = 98
+	x[1] = 93
+	x[2] = 77
+	x[3] = 82
+	x[4] = 83	
+	var total float64 = 0
+	for i, value := range x {
+		total += value
+		fmt.Println(i)
+	}
+	fmt.Println(total / float64(len(x)))
+}
 
 func mainArray() {
 	// arrays
