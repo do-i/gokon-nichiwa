a331b72f848733ab53b32fedbda61bf4c9a665e1
acorn <acorn@junk>
2015-09-13 23:34:18 -0500

    added more maps

--- hello.go
+++ hello.go
@@ -4,7 +4,8 @@ import "fmt"
 
 var x = [3]float64{1,2,3}
 func main() {
-	coolMap()
+	nestedMaps()
+	// coolMap()
 	// goodMap()
 	// slices()
 	// mainForLoopNoIndex() 
@@ -17,6 +18,56 @@ func main() {
 	// mainStdIn() 
 	// mainVariablesAndConstants() 
 } 
+
+func nestedMaps() {
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+		"name":"Hydrogen",
+		"state":"gas",
+		},
+		"He": map[string]string{
+		"name":"Helium",
+		"state":"gas",
+		},
+		"Li": map[string]string{
+		"name":"Lithium",
+		"state":"solid",
+		},		
+		"Be": map[string]string{
+		"name":"Beryllium",
+		"state":"solid",
+		},
+		"B": map[string]string{
+		"name":"Boron",
+		"state":"solid",
+		},
+		"C": map[string]string{
+		"name":"Carbon",
+		"state":"solid",
+		},
+		"N": map[string]string{
+		"name":"Nitrogen",
+		"state":"gas",
+		},
+		"O": map[string]string{
+		"name":"Oxygen",
+		"state":"gas",
+		},
+		"F": map[string]string{
+		"name":"Fluorine",
+		"state":"gas",
+		},
+		"Ne": map[string]string{
+		"name":"Neon",
+		"state":"gas",
+		},
+	}
+	// First try to get a value from map, then if it's 
+	// successful we run the code inside of the ok block.
+	if el, ok := elements["Li"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}		
+}
 
 func coolMap() {
 	elements := map[string]string {
