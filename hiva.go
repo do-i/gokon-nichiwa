545469c782450adc71501a297302a6c8f9b2ce25
Ninja <ninja@junk.net>
2015-09-25 19:54:36 -0500

    interfaces, embedded types, methods

--- hiva.go
+++ hiva.go
@@ -22,11 +22,15 @@ func shortList() {
 	child.value = 13
 
 	// another way to create Node object
-	// then assign address of grandChild object to the pointer
-	grandChild := Node{888, nil}
-	child.next = &grandChild
+	grandChild := Node{value:888, next:nil}
+	// another way to link by calling add method
+	child.add(grandChild)
 
-	printer(root)
+	// one way to print the list, pass to function
+	// printer(root)
+
+	// another way to print the list, by calling function
+	root.print()
 }
 
 func printer(list *Node) {
@@ -38,10 +42,81 @@ func printer(list *Node) {
 	fmt.Println(current.value)
 }
 
+// Ch 9.2 Methods
+func (node *Node) print() {
+	fmt.Println("Method to print.")
+	printer(node)
+}
+// pass by object vs pointer; would it be better to pass by pointer? What's memory usage?
+// pass by object copys object ??
+func (node *Node) add(child Node) {
+	node.next = &child
+}
+
+// Ch 9.2.2 Embedded Types:- inheritance
+type Person struct {
+	firstName string
+	lastName string
+}
+type Cyborg struct {
+	Person
+	model int32
+}
+func (p *Person) print() {
+	fmt.Printf("%s %s\n", p.firstName, p.lastName)
+}
+// func (cyborg *Cyborg) print() {
+// 	// cyborg.print()  // TODO how can I call parent method????
+// 	fmt.Println(cyborg.model)
+// }
+
+func embeddedTypesMain() {
+	person := new(Person)
+	person.firstName = "Kumi"
+	person.lastName = "Otowa"
+	person.print()
+
+	cyborg := new(Cyborg)
+	cyborg.firstName = "testujin"
+	cyborg.lastName = "28Go"
+	cyborg.model = 28
+	cyborg.print()
+}
+
+// 9.3 Interfaces
+type Animal interface {
+	Speak() string
+}
+type Dog struct {
+}
+func (dog Dog) Speak() string {
+	return "Woof!"
+}
+type Cat struct {
+}
+func (cat Cat) Speak() string {
+	return "Meow!"
+}
+type Dev struct {
+
+}
+func (dev Dev) Speak() string {
+	return "Ship it!"
+}
+
+func interfacesMain() {
+	animals := []Animal{Dog{}, Cat{}, Dev{}}
+	for _, animal := range animals {
+		fmt.Println(animal.Speak())
+	}
+}
+
 func main() {
 	mainHello()
 	mainNihao()
 	mainHachi()
 	fmt.Println("empty")
 	shortList()
+	embeddedTypesMain()
+	interfacesMain()
 }
